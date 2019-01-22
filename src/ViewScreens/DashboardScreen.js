cf4bffd26b907e0a3c3054d982859392439b68ba
Pasindu <pasinduath@gmail.com>
2019-01-22 18:32:02 +0530

    Update Dashboard View

--- src/ViewScreens/DashboardScreen.js
+++ src/ViewScreens/DashboardScreen.js
@@ -1,30 +1,243 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import ViewOrders from "../Views/Dashboard/viewOrders";
 import NewOrder from "../Views/Dashboard/newOrder"
 import EditOrder from "../Views/Dashboard/editOrder"
 import NavigationSimple from "../Util/Components/Navigation";
+import PageLoader from "../Util/Components/Pageloader";
+import {updateOrderItem, createNewOrder, deleteOrderData,deleteOrderItem} from "../RestServices/PushServices";
+import {getAllOrders, getAllItems,getOrder} from "../RestServices/PullServices";
+import {withRouter} from "react-router-dom";
 
 
 class DashboardScreen extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            liveComponent: <NewOrder/>,
+            liveComponent: <PageLoader/>,
+            componentUpdate:{}
         };
     }
 
-    async initialize() {
-            //this.props.history.push("/login")
-        }
 
     componentDidMount() {
-        //this.initialize().catch(()=>{ this.props.history.push("/error") })
+        this.initialize().catch(() => {
+                this.props.history.push("/error")
+            }
+        );
     }
+
+    async initialize() {
+        let orderResponse = null;
+        let orderContent = null;
+        await getOrder("A6002862447292428459AA5320207605687665475").then((result) => {
+            orderResponse = result['status'];
+            orderContent = result['content']
+        });
+        let itemResponse = null;
+        let itemContent = null;
+        await getAllItems().then((result) => {
+            itemResponse = result['status'];
+            itemContent = result['content']
+        });
+        switch (itemResponse) {
+            case 200:
+                this.setState({
+                    liveComponent: <EditOrder componentData={orderContent} secondaryComponentData={itemContent}
+                                              frontEndCommunicator={this.frontEndCommunicator}/>
+                });
+                break;
+            case 401:
+                this.props.history.push('/login');
+                break;
+            case 280:
+                this.props.history.push('/login');
+                break;
+            default:
+                this.errorScreen()
+        }
+        // this.setState({
+        //     liveComponent: <EditOrder componentData={[ {
+        //         "key": "Juston Bruen",
+        //         "text": "Juston Bruen",
+        //         "value": "juston_bruen"
+        //     }]} secondaryComponentData={ [ {
+        //         "key": "Juston Bruen",
+        //         "text": "Juston Bruen",
+        //         "value": "juston_bruen"
+        //     },
+        //         {
+        //             "key": "Pattie Simonis",
+        //             "text": "Pattie Simonis",
+        //             "value": "pattie_simonis"
+        //         },
+        //         {
+        //             "key": "June Schoen",
+        //             "text": "June Schoen",
+        //             "value": "june_schoen"
+        //         }]} frontEndCommunicator={this.frontEndCommunicator}/>
+        // });
+        // await getAllOrders().then((result) => {
+        //     response = result['status'];
+        //     content = result['content']
+        // });
+        // switch (response) {
+        //     case 200:
+        //         console.log(content)
+        //         this.setState({
+        //             liveComponent: <ViewOrders compoentData={content} frontEndCommunicator={this.frontEndCommunicator}/>
+        //         });
+        //         break;
+        //     case 401:
+        //         this.props.history.push('/login');
+        //         break;
+        //     case 280:
+        //         this.props.history.push('/error');
+        //         break;
+        //     default:
+        //         this.errorScreen()
+        // }
+    }
+
+
+    async editOrder(orderUpdate) {
+        let response = null;
+        let content = null;
+        await updateOrderItem(orderUpdate).then((result) => {
+            response = result['status'];
+            content = result['content']
+        });
+        switch (response) {
+            case 200:
+                this.setState({componentUpdate:{}})
+                break;
+            case 401:
+                this.props.history.push('/login');
+                break;
+            case 280:
+                this.props.history.push('/login');
+                break;
+            default:
+                this.errorScreen()
+        }
+
+
+    }
+
+    async newOrder() {
+
+        let orderResponse = null;
+        let orderContent = null;
+        this.setState({liveComponent: <PageLoader/>});
+        await createNewOrder().then((result) => {
+            orderResponse = result['status'];
+            orderContent = result['content']
+        });
+        switch (orderResponse) {
+            case 200:
+                this.initialize().catch(this.errorScreen);
+                break;
+            case 401:
+                this.props.history.push('/login');
+                break;
+            case 280:
+                this.props.history.push('/login');
+                break;
+            default:
+                this.errorScreen()
+        }
+        let itemResponse = null;
+        let itemContent = null;
+        await getAllItems().then((result) => {
+            itemResponse = result['status'];
+            itemContent = result['content']
+        });
+        switch (itemResponse) {
+            case 200:
+                this.setState({
+                    liveComponent: <NewOrder componentData={orderContent} secondryComponentData={itemContent} frontEndCommunicator={this.frontEndCommunicator}/>
+                });
+                break;
+            case 401:
+                this.props.history.push('/login');
+                break;
+            case 280:
+                this.props.history.push('/login');
+                break;
+            default:
+                this.errorScreen()
+        }
+
+
+    }
+
+    async updateOrder(orderData) {
+        this.setState({liveComponent: <PageLoader/>});
+        let response = null;
+        await updateOrderItem(orderData).then((result) => {
+            response = result['status']
+        });
+        switch (response) {
+            case 200:
+                this.initialize().catch(this.errorScreen);
+                break;
+            case 401:
+                this.props.history.push('/login');
+                break;
+            case 280:
+                this.props.history.push('/login');
+                break;
+            default:
+                this.errorScreen()
+        }
+    }
+
+
+
+    async deleteOrder(orderData) {
+        this.setState({liveComponent: <PageLoader/>});
+        let response = null;
+        await deleteOrderData(orderData).then((result) => {
+            response = result['status']
+        });
+        switch (response) {
+            case 200:
+                this.initialize().catch(this.errorScreen);
+                break;
+            case 401:
+                this.props.history.push('/login');
+                break;
+            case 280:
+                this.props.history.push('/login');
+                break;
+            default:
+                this.errorScreen()
+        }
+    }
+
 
     errorScreen = () => {
         this.props.history.push('/error')
     };
 
+
+    frontEndCommunicator = (command, ...args) => {
+        switch (command) {
+            case 'editOrder':
+                this.editOrder(args[0]).catch(this.errorScreen);
+                break;
+            case 'createOrder':
+                this.newOrder().catch(this.errorScreen);
+                break;
+            case 'updateOrder':
+                this.editOrder(args[0],args[1]).catch(this.errorScreen);
+                break;
+            case 'deleteOrder':
+                this.deleteOrder(args[0]).catch(this.errorScreen);
+                break;
+            default:
+                this.errorScreen();
+        }
+    };
 
 
     render() {
@@ -33,10 +246,10 @@ class DashboardScreen extends Component {
 
                 <NavigationSimple/>
                 <br/>
-                    {this.state.liveComponent}
+                {this.state.liveComponent}
             </div>
         );
     }
 }
 
-export default DashboardScreen;
+export default withRouter(DashboardScreen);
